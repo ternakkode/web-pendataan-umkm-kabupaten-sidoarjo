85fdc67bd773548b385e784247bd019ec7b19200
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-05-05 14:02:24 +0700

    test perubahan

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 
-
 Route::post('umkm/produk/image/upload', 'API\ProductImageController@upload');
 Route::patch('umkm/produk/{idProduk}/image/{idImage}', 'API\ProductImageController@setPrimary');
 Route::delete('umkm/produk/{idProduk}/image/{idImage}', 'API\ProductImageController@delete');
